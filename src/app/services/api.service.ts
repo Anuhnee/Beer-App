d40cc7c1e1e15494f3dccb34f3f3be5edf8b06d6
Anthony Perez <antrp93@gmail.com>
2020-04-20 18:45:26 -0500

    add services & httpclient

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http/http';
+import { IBeer } from '../interfaces/ibeer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private BEER_URL = " https://api.punkapi.com/v2/beers";
+  constructor(private httpClient: HttpClient) { }
+
+ async get(){
+  return this.httpClient.get<IBeer>(this.BEER_URL).toPromise();
+ }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http/http';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
+
+  async getBeers(){
+    return await this.apiService.get();
+  }
 }
